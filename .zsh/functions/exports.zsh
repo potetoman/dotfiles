b6b67aa81a6ad96aa34a115cf220187774a41193
shota sato <shota-s@ecei.tohoku.ac.jp>
2016-07-11 22:24:15 +0900

    add mecab lib path

--- .zsh/functions/exports.zsh
+++ .zsh/functions/exports.zsh
@@ -14,6 +14,11 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # server mecab path
 export PATH=/opt/local/bin:$PATH
 
+export PATH=/opt/local/lib:$PATH
+
+# lsコマンド時、自動で色がつく(ls -Gのようなもの？)
+>>>>>>> add mecab lib path
+
 # lsコマンド時、自動で色がつく(ls -Gのようなもの？)
 export CLICOLOR=true
 
