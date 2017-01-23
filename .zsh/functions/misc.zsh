34e1a4c055c2ce7320666e1792152e9f17e0ad9a
Shota Sato <shota-s@ecei.tohoku.ac.jp>
2017-01-23 11:36:53 +0900

    Add cdr settings

--- .zsh/functions/misc.zsh
+++ .zsh/functions/misc.zsh
@@ -11,3 +11,13 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
 eval "$(pyenv init -)"
+
+# cdr settings
+if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
+    autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+    add-zsh-hook chpwd chpwd_recent_dirs
+    zstyle ':completion:*' recent-dirs-insert both
+    zstyle ':chpwd:*' recent-dirs-default true
+    zstyle ':chpwd:*' recent-dirs-max 1000
+    zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/chpwd-recent-dirs"
+fi
