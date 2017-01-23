b7f61fa183af488cde31ad0d959b323f62a78ccd
Shota Sato <shota-s@ecei.tohoku.ac.jp>
2017-01-23 11:08:09 +0900

    Add coreutils and gnu-sed path

--- .zsh/functions/exports.zsh
+++ .zsh/functions/exports.zsh
@@ -4,5 +4,12 @@ export PATH=/usr/local/bin:$PATH
 # tex path
 export PATH=/Library/Tex/texbin/:$PATH
 
+# coreutils and gnu-sed path !MAC only
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+
 # lsコマンド時、自動で色がつく(ls -Gのようなもの？)
 export CLICOLOR=true
+
+# neovim home_path
+export XDG_CONFIG_HOME=$HOME/.config
