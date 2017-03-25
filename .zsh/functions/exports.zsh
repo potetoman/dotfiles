cc36a376853b50d3c0c75ab42de55037358495ee
shota sato <shota-s@ecei.tohoku.ac.jp>
2017-03-25 10:28:48 +0900

    add cuda path

--- .zsh/functions/exports.zsh
+++ .zsh/functions/exports.zsh
@@ -7,7 +7,9 @@ export MECAB_PATH=/opt/local/lib/libmecab.so.2
 export LD_LIBRARY_PATH=/opt/local/lib/:$LD_LIBRARY_PATH
 
 # cuda path
+export PATH=/usr/local/cuda/bin:$PATH
 export CUDA_PATH=/usr/local/cuda
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 # lsコマンド時、自動で色がつく(ls -Gのようなもの？)
 export CLICOLOR=true
