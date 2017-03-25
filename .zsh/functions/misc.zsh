45e19a6149b6f8578eb9438faeca6211eefcc84c
shota sato <shota-s@ecei.tohoku.ac.jp>
2017-03-25 10:29:54 +0900

    add func which changes dev_env depending on hostname

--- .zsh/functions/misc.zsh
+++ .zsh/functions/misc.zsh
@@ -11,3 +11,13 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
 eval "$(pyenv init -)"
+
+# conda change
+if [ $(hostname) = "gunfire" ]; then
+    source ${PYENV_ROOT}/versions/anaconda-4.0.0/bin/activate chainer_gunfire3
+    export PYTHONPATH=/home/shota-s/.pyenv/versions/anaconda-4.0.0/envs/chainer_gunfire3/lib/python3.5/site-packages
+elif [ $(hostname) = "maitai" ]; then
+    source ${PYENV_ROOT}/versions/anaconda-4.0.0/bin/activate chainer_maitai3
+    export PYTHONPATH=/home/shota-s/.pyenv/versions/anaconda-4.0.0/envs/chainer_maitai3/lib/python3.5/site-packages
+else
+fi
